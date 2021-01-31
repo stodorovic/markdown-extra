155cd8f85b4adc15e4ea8d543394966aff34eaeb
Sasa Todorovic <sasa.todorovic@gmx.com>
2021-02-01 00:08:48 +0100

    Fix tests...

--- tests/unit/tests/Markdown_Test.php
+++ tests/unit/tests/Markdown_Test.php
@@ -18,7 +18,7 @@ class Tests_Markdown extends Markdown_Unit_Test_Case {
 			array(
 				'post_title'   => 'Test Post',
 				'post_type'    => 'post',
-				'post_content' => 'This is content',
+				'post_content' => "# This is an H1\n\n* Red\n* Green\n* Blue\n",
 				'post_status'  => 'publish',
 			)
 		);
@@ -30,10 +30,12 @@ class Tests_Markdown extends Markdown_Unit_Test_Case {
 	 *
 	 */
 	public function test_filter() {
+		global $post;
+
 		$this->go_to( get_permalink( $this->_post_id ) );
 		
 		//var_dump( $GLOBALS['post'] );
-		var_dump( apply_filters( 'the_content', '# This is an H1\n\n## This is an H2\n' ) );
+		var_dump( apply_filters( 'the_content', $post->post_content ) );
 		//var_dump( the_content() );
 	}
 }
